6640aaa6c7c9ef6b351fe1db97ee94293c0410ae
Sumit vikram singh <sumitvikramsingh46@gmail.com>
2024-09-04 15:02:04 +0530

    changes

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -71,8 +71,8 @@ export default function PostForm({ post }) {
     }, [watch, slugTransform, setValue]);
 
     return (
-        <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
-            <div className="w-2/3 px-2">
+        <form onSubmit={handleSubmit(submit)} className="grid-cols-2 ">
+            <div className=" px-2">
                 <Input
                     label="Title :"
                     placeholder="Title"
@@ -90,7 +90,7 @@ export default function PostForm({ post }) {
                 />
                 <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
             </div>
-            <div className="w-1/3 px-2">
+            <div className=" px-2">
                 <Input
                     label="Featured Image :"
                     type="file"
